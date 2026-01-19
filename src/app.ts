1c0ff2fcc6ae508dda7783ddc54445e8e81fe33f
abujaher5 <abujaher5120@gmail.com>
2026-01-19 09:35:45 +0600

    Initial setup and post api integrate

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Application } from "express";
+import { postRouter } from "./modules/post/post.router";
+const app: Application = express();
+
+app.use(express.json());
+app.use("/posts", postRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hello, World! This is the Express app.");
+});
+
+export default app;
